56e77da94c15a11ce6094371fd01fab10ee50da2
Afroza2 <afroza.nowshin@gmail.com>
2022-06-24 23:20:43 +0600

    adding public api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import Home from "./pages/home";
-import About from "./pages/about";
-import Contact from "./pages/contact";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 
 class App extends Component {
@@ -13,21 +11,22 @@ class App extends Component {
   render() {
     return (
       <div className="body-wrapper">
-        <BrowserRouter>
-              <Switch>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-                <Route path="/about">
-                  <About />
-                </Route>
+          <Home/>
+        {/*<BrowserRouter>*/}
+        {/*      <Switch>*/}
+        {/*        <Route exact path="/">*/}
+        {/*          <Home />*/}
+        {/*        </Route>*/}
+        {/*        <Route path="/about">*/}
+        {/*          <About />*/}
+        {/*        </Route>*/}
 
-                <Route path="/contact">
-                  <Contact />
-                </Route>
+        {/*        <Route path="/contact">*/}
+        {/*          <Contact />*/}
+        {/*        </Route>*/}
 
-              </Switch>
-            </BrowserRouter>
+        {/*      </Switch>*/}
+        {/*    </BrowserRouter>*/}
       </div>
     );
   }
